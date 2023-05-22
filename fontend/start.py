e3ebbd803d09a612d03217dbef24cc124dfc262b
PuunKotHodJumpFuc <105053170+puun555@users.noreply.github.com>
2023-05-22 17:46:14 +0700

    sss
    sdsd

--- fontend/start.py
+++ fontend/start.py
@@ -1,1 +1,11 @@
 # start
+
+import tkinter as tk
+
+def print_something(text):
+    print(text)
+
+app = tk.Tk()
+button = tk.Button(text='click me!', command= lambda :print_something('print this'))
+button.pack()
+app.mainloop()
