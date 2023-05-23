5cede8fce3d5a5828d42b4250f0f2a4af95037b6
Chayanont Supakit <64070017@kmitl.ac.th>
2023-05-24 00:40:59 +0700

    Ping and ShowIP

--- backend/controller.py
+++ backend/controller.py
@@ -1,1 +1,20 @@
 # controller
+from netmiko import ConnectHandler
+
+cisco_ios = {
+    "device_type": "cisco_ios_telnet",
+    "host": "172.30.230.46",
+    "port" : "5003"
+}
+cmd = ConnectHandler(**cisco_ios)
+def enable():
+    cmd.enable()
+    
+def show_ip_interface():
+    result = cmd.send_command("show ip int br")
+    print(result)
+    
+def ping():
+    ip = input("Destination IP Address : ")
+    result = cmd.send_command("ping %s" %ip)
+    print(result)
--- fontend/start.py
+++ fontend/start.py
@@ -1,1 +1,17 @@
 # start
+
+import tkinter as tk
+
+import sys
+sys.path.append("backend")
+import controller
+
+def print_something(text):
+    print(text)
+
+app = tk.Tk()
+button = tk.Button(text='Ping!', command= lambda :controller.ping())
+button2 = tk.Button(text='Show IP Interface!', command= lambda :controller.show_ip_interface())
+button.pack()
+button2.pack()
+app.mainloop()
